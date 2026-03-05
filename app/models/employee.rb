f04ab2c8901413dd295ba3801544bb9b06f7891c
v_napons <napons@setth.onmicrosoft.com>
2026-03-06 00:57:16 +0700

    feat: Add validate employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -11,4 +11,8 @@
 #  updated_at :datetime         not null
 #
 class Employee < ApplicationRecord
+    validates :name, presence: true
+    validates :last_name, presence: true
+    validates :position, presence: true
+    validates :salary, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
