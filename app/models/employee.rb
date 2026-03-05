0f5f2778438887d117dee340dd8b7cafa0e05af3
v_napons <napons@setth.onmicrosoft.com>
2026-03-06 00:54:47 +0700

    feat: Add install annotate

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id         :integer          not null, primary key
+#  last_name  :text
+#  name       :text
+#  position   :text
+#  salary     :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Employee < ApplicationRecord
 end
--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id         :integer          not null, primary key
+#  last_name  :text
+#  name       :text
+#  position   :text
+#  salary     :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class EmployeeTest < ActiveSupport::TestCase
