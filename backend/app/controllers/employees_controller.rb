d322e182e41f51dde3ef481e68cf0a3ff3cf2362
v_napons <napons@setth.onmicrosoft.com>
2026-03-06 21:48:25 +0700

    feat: Add insert update view employees

--- backend/app/controllers/employees_controller.rb
+++ backend/app/controllers/employees_controller.rb
@@ -4,9 +4,25 @@ class EmployeesController < ApplicationController
     render json: employees
   end
 
+  def show
+    employee = Employee.find(params[:id])
+    render json: employee
+  end
+
+  def create
+    employee = Employee.create!(employee_params)
+    render json: employee
+  end
+
+  def update
+    employee = Employee.find(params[:id])
+    employee.update!(employee_params)
+    render json: employee
+  end
+
   private
 
   def employee_params
-    params.require(:employee).permit(:name, :salary, :position)
+    params.require(:employee).permit(:name, :last_name, :position, :salary)
   end
 end
